2773c659969c34eb005cb58958ebd260de1e0d3e
Paul Steele <60023560+ManOfSteele18@users.noreply.github.com>
2021-02-08 17:24:43 -0500

    Update index.js
    Added eating behavior for dots and power pills, display score, added game win condition

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function gameOver(pacman, grid) {
 function checkCollision(pacman, ghosts) {
     const collidedGhost = ghosts.find( ghost => pacman.pos === ghost.pos);
     if (collidedGhost) {
-        if (pacman.powerPillActive) {
+        if (pacman.powerPill) {
             gameBoard.removeObject(collidedGhost.pos, [
                 OBJECT_TYPE.GHOST,
                 OBJECT_TYPE.SCARED,
@@ -58,6 +58,42 @@ function gameLoop(pacman, ghosts) {
 
     ghosts.forEach((ghost) => gameBoard.moveCharacter(ghost));
     checkCollision(pacman, ghosts);
+
+    // Check if pacman eats a dot
+    if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.DOT)) {
+        gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.DOT]);
+        gameBoard.dotCount--;
+        score += 10;
+    }
+
+    // Check if pacman eats a power pill
+    if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.PILL)) {
+        gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PILL]);
+
+        pacman.powerPill = true;
+        score += 50;
+
+        clearTimeout(powerPillTimer);
+        powerPillTimer = setTimeout(
+            () => (pacman.powerPill = false),
+            POWER_PILL_TIME
+        );
+    }
+
+    // Change ghost scare mode depending on power pill
+    if (pacman.powerPill !== powerPillActive) {
+        powerPillActive = pacman.powerPill;
+        ghosts.forEach((ghost) => (ghost.isScared = pacman.powerPill));
+    }
+
+    // Check if all dots have been eaten
+    if (gameBoard.dotCount === 0) {
+        gmaeWin = true;
+        gameOver(pacman, ghosts);
+    }
+    
+    // Show the score
+    scoreTable.innerHTML = score;
 }
 
 function startGame() {
